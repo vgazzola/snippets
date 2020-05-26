ef116f9d0064c629d8b10201979b817617b41559
Valentin Gazzola <vag@open.ch>
2020-05-26 16:41:06 +0200

    add an example

--- hadoop/check_ports_from_client.sh
+++ hadoop/check_ports_from_client.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#EXAMPLE
+#HIVE="10.12.0.10 10.12.2.10" NAMENODES="10.12.0.10 10.12.2.10" DATANODES="10.12.0.11 10.12.0.12 10.12.2.11 10.12.2.12 10.12.4.11 10.12.4.12" ZOOKEEPER="10.12.0.10 10.12.2.10 10.12.4.10" YARN_MASTER="10.12.0.10 10.12.2.10" ./check_ports_from_client.sh
+
 # requires nmap. I don't want to have fun with telnet :)
 PORT_SCANNER="nmap"
 CHECK_TCP="${PORT_SCANNER} -Pn"
@@ -32,7 +35,7 @@ fi
 
 
 scan "HIVE" 9083,10000 "${HIVE}"
-scan "NAMENODES" 50070 "${NAMENODES}"
+scan "NAMENODES" 50070,8020 "${NAMENODES}"
 scan "DATANODES" 50075 "${DATANODES}"
 scan "ZOOKEEPER" 2181 "${ZOOKEEPER}"
 scan "YARN" 8050,8025,8088,8090 "${YARN_MASTER}"
